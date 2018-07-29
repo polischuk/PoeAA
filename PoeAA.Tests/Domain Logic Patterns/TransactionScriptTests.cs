52c4cc7a5cd8cd65f3f1a2634fbca79263189321
Artem Polischuk <polischukao@gmail.com>
2018-07-30 01:06:28 +0300

    Initial commit
    Added project and TransactionScript implementation

---
+++ PoeAA.Tests/Domain Logic Patterns/TransactionScriptTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PoeAA.Domain_Logic_Patterns;
+using Xunit;
+
+namespace PoeAA.Tests.Domain_Logic_Patterns
+{
+    public class TransactionScriptTests
+    {
+        private string connString =
+            "Data Source=localhost;Initial Catalog=test;Integrated Security=True;MultipleActiveResultSets=True";
+        [Fact]
+        public void ExecuteInsertCommand_WithValidData_ShouldInsertEntityToDb()
+        {
+            var ts = new TransactionScript<TransactionScriptModel>();
+            ts.SetCommand(new InsertCommand(connString));
+            ts.ExecuteCommand(new TransactionScriptModel
+            {
+                Status = "Done",
+                Id = Guid.NewGuid()
+            });
+        }
+        [Fact]
+        public void ExecuteInsertCommand_WithBadStatus_ShouldThrowException()
+        {
+            var ts = new TransactionScript<TransactionScriptModel>();
+            ts.SetCommand(new InsertCommand(connString));
+            Assert.Throws<Exception>(() => ts.ExecuteCommand(new TransactionScriptModel
+            {
+                Status = "Fake",
+                Id = Guid.NewGuid()
+            }));
+        }
+    }
+}
---
+++ PoeAA/Domain Logic Patterns/TransactionScript.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Transactions;
+using Dapper;
+
+namespace PoeAA.Domain_Logic_Patterns
+{
+    public class TransactionScript<T>
+    {
+        private ICommand<T> _command;
+
+        public void SetCommand(ICommand<T> command)
+        {
+            _command = command;
+        }
+
+        public void ExecuteCommand(T model)
+        {
+            _command.Execute(model);
+        }
+    }
+
+    public interface ICommand<in T>
+    {
+        void Execute(T model);
+    }
+
+    public class InsertCommand : ICommand<TransactionScriptModel>
+    {
+        private readonly string _connectionString;
+
+        public InsertCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Execute(TransactionScriptModel model)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                db.Open();
+                using (IDbTransaction transaction = db.BeginTransaction(System.Data.IsolationLevel.ReadUncommitted))
+                {
+                    try
+                    {
+                        db.CreateCommand().Transaction = transaction;
+                        var sqlQuery = "INSERT INTO TransactionScript (Id,Status) VALUES(@Id, @Status)";
+                        db.Execute(sqlQuery, model, transaction);
+                        DoStuff(model);
+                        transaction.Commit();
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+        public void DoStuff(TransactionScriptModel model)
+        {
+            if (model == null || model.Status == "Fake")
+                throw new Exception("Bad data");
+        }
+    }
+
+    public class TransactionScriptModel
+    {
+        public Guid Id { get; set; }
+        public string Status { get; set; }
+    }
+}
