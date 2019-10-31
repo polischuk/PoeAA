695f51f1bcaf775b94a7f43fb0acaadd3ae5b910
Artem Polischuk <polischukao@gmail.com>
2019-11-01 00:36:55 +0200

    UOW pattern added

--- PoeAA/Object-Relational Metadata Mapping Patterns/GenericRepository.cs
+++ PoeAA/Object-Relational Metadata Mapping Patterns/GenericRepository.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace PoeAA
 {
